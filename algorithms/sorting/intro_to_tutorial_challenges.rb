fb5ca02ebfe757f01a6edf7a86fea57ef978ba70
Anthony Nabuurs <kivohin@protonmail.com>
2016-08-31 23:35:53 -0400

    Solve this challenge with .each_index.

--- algorithms/sorting/intro_to_tutorial_challenges.rb
+++ algorithms/sorting/intro_to_tutorial_challenges.rb
@@ -1,6 +1,14 @@
+require 'pry'
+
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
-V = gets.to_i
+v = gets.to_i
 n = gets.to_i
 array = gets.split(" ")
+position = 0
 
+array.each_index do |num|
+	position = num if array[num].to_i == v	
+end
+
+p position
