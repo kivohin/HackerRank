6459ded0efe2080c272808c5bc427bb9a3999216
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-02 15:03:46 -0400

    Create for loop and if statement.

--- algorithms/sorting/insertion_sort_part_1/exercise.js
+++ algorithms/sorting/insertion_sort_part_1/exercise.js
@@ -1,17 +1,26 @@
 function processData(input) {
 	var e = input[input.length - 1];
 
-	
+	for (i = array.length - 1; i < input.length; i--) {
+		if (array[i] < array[i - 1]) {
+			array[i] = array[i - 1]
+		};
+	};
 } 
 
-process.stdin.resume();
-process.stdin.setEncoding("ascii");
-_input = "";
+var i, array;
+array = [1, 3, 4, 2]
 
-process.stdin.on("data", function (input) {
-    _input += input;
-});
+processData(array);
 
-process.stdin.on("end", function () {
-   processData(_input);
-});
+// process.stdin.resume();
+// process.stdin.setEncoding("ascii");
+// _input = "";
+
+// process.stdin.on("data", function (input) {
+//     _input += input;
+// });
+
+// process.stdin.on("end", function () {
+//    processData(_input);
+// });
