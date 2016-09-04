9d7faa512eafc1095d27d06fb86097ba02cd8f6a
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-04 15:29:12 -0400

    Solve insertion sort part 1 challenge.

--- algorithms/sorting/insertion_sort_part_1/exercise.js
+++ algorithms/sorting/insertion_sort_part_1/exercise.js
@@ -9,19 +9,38 @@ function processData(input) {
 
   var e = arr[n - 1];
 
-	for (var i = n - 1; i > 0 ; i--) {
+	for (var i = n - 1; i >= 0 ; i--) {
 		if (e < arr[i - 1]) {
 			arr[i] = arr[i - 1];
-   		console.log(arr.toString());
-		} else {
+   		var result = "";
+   		arr.forEach(function(num) {
+   			result = result + num.toString() + " ";
+   		});
+   		console.log(result);
+		}
+		else if (e > arr[i - 1] && e < arr[i + 1]) {
 			arr[i] = e;
-      console.log(arr.toString());
+			result = "";
+			arr.forEach(function(num) {
+   			result = result + num.toString() + " ";
+   		});
+      console.log(result);
+		}
+		else if (e < arr[i + 1]) {
+			arr[i] = e;
+			result = "";
+			arr.forEach(function(num) {
+   			result = result + num.toString() + " ";
+   		});
+      console.log(result);
 		};
 	};
-}  
+}
 
 var string;
-string = "5\n2 4 6 8 3"
+// string = "5\n2 4 6 8 3"
+// string = "14\n1 3 5 9 13 22 27 35 46 51 55 83 87 23"
+string = "10\n2 3 4 5 6 7 8 9 10 1"
 
 processData(string);
 
