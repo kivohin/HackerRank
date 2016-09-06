8bd6ffec6365e56111c318a37e5de7e8938c31b2
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-06 11:42:59 -0400

    Insert correct code for insertion sort part 2 challenge, which contained incorrect code.

--- algorithms/sorting/insertion_sort_part_2/exercise.js
+++ algorithms/sorting/insertion_sort_part_2/exercise.js
@@ -7,51 +7,33 @@ function processData(input) {
   // Other way of converting strings to integers
   // var arr = lines[1].split(" ").map(Number);
 
-  var e = arr[n - 1];
+  for (var i = 1; i < n; i++) {
+    position = i;
+    shift_number = arr[i];
+    while (shift_number < arr[position - 1]) {
+      arr[position] = arr[position - 1];
+      arr[position - 1] = shift_number;
 
-  for (var i = n - 1; i >= 0 ; i--) {
-    if (e < arr[i - 1]) {
-      arr[i] = arr[i - 1];
-      var result = "";
-      arr.forEach(function(num) {
-        result = result + num.toString() + " ";
-      });
-      console.log(result);
-    }
-    else if (e > arr[i - 1] && e < arr[i + 1]) {
-      arr[i] = e;
-      result = "";
-      arr.forEach(function(num) {
-        result = result + num.toString() + " ";
-      });
-      console.log(result);
-    }
-    else if (e < arr[i + 1]) {
-      arr[i] = e;
-      result = "";
-      arr.forEach(function(num) {
-        result = result + num.toString() + " ";
-      });
-      console.log(result);
+      if (position > 0) {
+        position--;
+      }
     };
+
+    result = "";
+    arr.forEach(function(num) {
+      result = result + num.toString() + " ";
+    });
+    console.log(result);
   };
-}
+};
 
-var string;
-// string = "5\n2 4 6 8 3"
-// string = "14\n1 3 5 9 13 22 27 35 46 51 55 83 87 23"
-string = "10\n2 3 4 5 6 7 8 9 10 1"
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
 
-processData(string);
-
-// process.stdin.resume();
-// process.stdin.setEncoding("ascii");
-// _input = "";
-
-// process.stdin.on("data", function (input) {
-//     _input += input;
-// });
-
-// process.stdin.on("end", function () {
-//    processData(_input);
-// });
+process.stdin.on("end", function () {
+   processData(_input);
+});
